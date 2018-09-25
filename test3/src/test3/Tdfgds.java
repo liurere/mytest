1cb0a539b36fff9d67291e1ed8c3ecfbf864a34c
liuxiangxian <liu.xiangxian@qq.com>
2018-09-25 11:33:27 +0800

    ew

--- test3/src/test3/Tdfgds.java
+++ test3/src/test3/Tdfgds.java
@@ -7,6 +7,11 @@ public class Tdfgds {
 		System.out.println("hello worled1");
 		System.out.println("hello worled2");
 		System.out.println("hello worled3");
+		
+	}
+	
+	public void a() {
+		
 	}
 	
 }
