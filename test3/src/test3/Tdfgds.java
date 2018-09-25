5433db1e455fa2e232ff338942170bfffa09eca8
liuxiangxian <liu.xiangxian@qq.com>
2018-09-25 10:51:05 +0800

    r

--- test3/src/test3/Tdfgds.java
+++ test3/src/test3/Tdfgds.java
@@ -4,7 +4,7 @@ public class Tdfgds {
   
 	public static void main(String[] args) {
 		System.out.println("323fr");
-		System.out.println("hello world");
+		System.out.println("hello worled");
 	}
 	
 }
